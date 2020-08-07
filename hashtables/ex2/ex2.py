92213a8b665e561b6b88038e5fc8b0af7baf4ab6
Issac Moreno <issaxavier909@gmail.com>
2020-08-07 08:52:46 -0700

    ex2 - MVP

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
-    return route
+    cache = {}
+    flight = [None] * length
+    
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+        
+    destination = cache["NONE"]
+    
+    for trip in range(length):
+        flight[trip] = destination
+        destination = cache[destination]
+        
+    return flight
